e16b5d93eef0f94c4a52ef913b15fd27140ebdcc
Alexandre Moreaux <alexandre.moreaux@outlook.com>
2023-10-16 14:03:44 +0200

    Update AVD Image.ps1

--- AVD Image.ps1
+++ AVD Image.ps1
@@ -3,20 +3,20 @@
 ##############
 $AzureSubscriptionName = "MSDN FR 2"
 $ResourceLocation = "westeurope"
-$NameoftheAvdImageCustomRole = "Azure Virtual Desktop Image Custom Role3"
-$UserManagedIdName = "My_AVD_Images_ID"
+$NameoftheAvdImageCustomRole = "Azure Virtual Desktop Image Custom Role4"
+$UserManagedIdName = "MyManagedIdentity"
 $AVDImageResourceGroup = "My_AVD_Images"
 $AvdAzureComputeGalleryName = "My_AVD_Azure_Compute_Gallery"
 
 ########################################################
 # Github repository where the script will be download #
 ########################################################
-$AvdImageCustomRolegithubRawUrl = "https://github.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/blob/main/AVD-Image-Custom-role.json"
-$AvdImageUserManagedIdentitygithubRawUrl = "https://github.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/blob/main/AVD-Image-User-Managed-identity.json"
+$AvdImageCustomRolegithubRawUrl = "https://raw.githubusercontent.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/main/AVD-Image-Custom-role.json"
+$AvdImageUserManagedIdentitygithubRawUrl = "https://raw.githubusercontent.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/main/AVD-Image-User-Managed-identity.json"
 $AvdAzureComputeGalleryRawUrl = "https://raw.githubusercontent.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/main/AVD-Azure-Compute-Gallery.json"
-$AvdImageCustomRoleOutputFile = "AVD-Image-Custom-role.json"
-$AvdImageUserManagedIdentityOutputFile = "AVD-Image-User-Managed-identity.json"
-$AvdAzureComputeGalleryOutputFile = "AVD-Azure-Compute-Gallery.json"
+$AvdImageCustomRoleOutputFile = "C:\AvdImage\AVD-Image-Custom-role.json"
+$AvdImageUserManagedIdentityOutputFile = "C:\AvdImage\AVD-Image-User-Managed-identity.json"
+$AvdAzureComputeGalleryOutputFile = "C:\AvdImage\AVD-Azure-Compute-Gallery.json"
 
 ##########################
 # Create Temp Directory #
@@ -33,7 +33,6 @@ if (-not (Test-Path $folderPath)) {
 ###########################################################
 # Download the JSON file from GitHub and save it locally #
 ###########################################################
-Set-Location $folderPath
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 Invoke-WebRequest -Uri $AvdImageCustomRolegithubRawUrl -OutFile $AvdImageCustomRoleOutputFile
 Invoke-WebRequest -Uri $AvdImageUserManagedIdentitygithubRawUrl -OutFile $AvdImageUserManagedIdentityOutputFile
@@ -66,43 +65,40 @@ Register-AzResourceProvider -ProviderNamespace Microsoft.DesktopVirtualization
 #############################################
 # Modify the Json Script with the Variable #
 #############################################
-$jsonContent = Get-Content -Path C:\AvdImage\AVD-Image-Custom-role.json | ConvertFrom-Json
+$jsonContent = Get-Content -Path $AvdImageCustomRoleOutputFile | ConvertFrom-Json
 $jsonContent.AssignableScopes = $jsonContent.AssignableScopes -replace "{subscriptionId}", $SubscriptionId
 $jsonContent.Name = $NameoftheAvdImageCustomRole
-$jsonContent | ConvertTo-Json | Set-Content -Path C:\AvdImage\AVD-Image-Custom-role.json
+$jsonContent | ConvertTo-Json | Set-Content -Path $AvdImageCustomRoleOutputFile
 
-$jsonContent2 = Get-Content -Path C:\AvdImage\AVD-Image-User-Managed-identity.json | ConvertFrom-Json
+$jsonContent2 = Get-Content -Path $AvdImageUserManagedIdentityOutputFile | ConvertFrom-Json
 $jsonContent2.resources[0].name = $UserManagedIdName
 $jsonContent2.resources[0].location = $ResourceLocation
 
-$jsonContent3 = Get-Content -Path C:\AvdImage\AVD-Azure-Compute-Gallery.json | ConvertFrom-Json
+$jsonContent3 = Get-Content -Path $AvdAzureComputeGalleryOutputFile | ConvertFrom-Json
 $jsonContent3.resources[0].name = $AvdAzureComputeGalleryName
 $jsonContent3.resources[0].location = $ResourceLocation
 
 ######################################
 # Creation of the Azure Custom Role #
 ######################################
-New-AzRoleDefinition -InputFile C:\AVD-Image-Custom-role.json
+New-AzRoleDefinition -InputFile $AvdImageCustomRoleOutputFile
 
 ##########################################
 # Creation of the User managed identity #
 ##########################################
 New-AzResourceGroup -Name $AVDImageResourceGroup -Location $ResourceLocation
-New-AzResourceGroupDeployment -ResourceGroupName $AVDImageResourceGroup -TemplateFile C:\AVD-Image-User-Managed-identity.json
+New-AzResourceGroupDeployment -ResourceGroupName $AVDImageResourceGroup -TemplateFile $AvdImageUserManagedIdentityOutputFile
 
 ###############################################
 # Assign the Custom Role the User Managed Id #
 ###############################################
-Install-Module Az.ManagedServiceIdentity -Force
-Import-Module Az.ManagedServiceIdentity -Force
 $AvdImageCustomRole = Get-AzRoleDefinition $NameoftheAvdImageCustomRole
 $userManagedIdentity = Get-AzUserAssignedIdentity -ResourceGroupName $AVDImageResourceGroup -Name $UserManagedIdName
-$UserManagedIdentityId = $userManagedIdentity.Id
-$AvdImageCustomRoleScope = "/subscriptions/$SubscriptionId/resourceGroups/$UserManagedIDResourceGroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$UserManagedIdName"
+$UserManagedIdentityId = $userManagedIdentity.PrincipalId
+$AvdImageCustomRoleScope = "/subscriptions/$SubscriptionId/resourceGroups/$AVDImageResourceGroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$UserManagedIdName"
 New-AzRoleAssignment -ObjectId $UserManagedIdentityId -RoleDefinitionName $AvdImageCustomRole.Name -Scope $AvdImageCustomRoleScope
 
 ##########################################
 # Creation of the Azure Compute Gallery #
 ##########################################
-New-AzResourceGroup -Name $AVDImageResourceGroup -Location $ResourceLocation
-New-AzResourceGroupDeployment -ResourceGroupName $AVDImageResourceGroup -TemplateFile C:\AVD-Azure-Compute-Gallery
+New-AzResourceGroupDeployment -ResourceGroupName $AVDImageResourceGroup -TemplateFile $AvdAzureComputeGalleryOutputFile
