1f96cca10952e0f8c2289fcad36a46cd2aca617c
Alexandre Moreaux <alexandre.moreaux@outlook.com>
2023-10-16 11:53:24 +0200

    Update AVD Image.ps1

--- AVD Image.ps1
+++ AVD Image.ps1
@@ -1,25 +1,47 @@
-#Azure Subscription Name
-$AzureSubscriptionName = ""
+##############
+# Variables #
+##############
+$AzureSubscriptionName = "MSDN FR 2"
 $ResourceLocation = "westeurope"
 $NameoftheAvdImageCustomRole = "Azure Virtual Desktop Image Custom Role3"
 $UserManagedIdName = "My_AVD_Images_ID"
 $AVDImageResourceGroup = "My_AVD_Images"
 $AvdAzureComputeGalleryName = "My_AVD_Azure_Compute_Gallery"
 
-#Github repository where the script will be download
+########################################################
+# Github repository where the script will be download #
+########################################################
 $AvdImageCustomRolegithubRawUrl = "https://github.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/blob/main/AVD-Image-Custom-role.json"
 $AvdImageUserManagedIdentitygithubRawUrl = "https://github.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/blob/main/AVD-Image-User-Managed-identity.json"
 $AvdAzureComputeGalleryRawUrl = "https://raw.githubusercontent.com/AlexandreMoreaux/Azure-Virtual-Desktop-Image/main/AVD-Azure-Compute-Gallery.json"
+$AvdImageCustomRoleOutputFile = "AVD-Image-Custom-role.json"
+$AvdImageUserManagedIdentityOutputFile = "AVD-Image-User-Managed-identity.json"
+$AvdAzureComputeGalleryOutputFile = "AVD-Azure-Compute-Gallery.json"
 
-# Download the JSON file from GitHub and save it locally
+##########################
+# Create Temp Directory #
+##########################
+$folderPath = "C:\AvdImage"
+
+if (-not (Test-Path $folderPath)) {
+    New-Item -ItemType Directory -Path $folderPath
+    Write-Host "Folder created: $folderPath"
+} else {
+    Write-Host "Folder already exists: $folderPath"
+}
+
+###########################################################
+# Download the JSON file from GitHub and save it locally #
+###########################################################
+Set-Location $folderPath
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+Invoke-WebRequest -Uri $AvdImageCustomRolegithubRawUrl -OutFile $AvdImageCustomRoleOutputFile
+Invoke-WebRequest -Uri $AvdImageUserManagedIdentitygithubRawUrl -OutFile $AvdImageUserManagedIdentityOutputFile
+Invoke-WebRequest -Uri $AvdAzureComputeGalleryRawUrl -OutFile $AvdAzureComputeGalleryOutputFile
 
-#Change the letters by the letter you want to use 
-Invoke-WebRequest -Uri $AvdImageCustomRolegithubRawUrl -OutFile C:\AVD-Image-Custom-role.json
-Invoke-WebRequest -Uri $AvdImageUserManagedIdentitygithubRawUrl -OutFile C:\AVD-Image-User-Managed-identity.json
-Invoke-WebRequest -Uri $AvdAzureComputeGalleryRawUrl -OutFile C:\AVD-Azure-Compute-Gallery
-
-#Connect to the Azure Account and Azure AD
+##################################
+# Connect to Azure and Azure AD #
+##################################
 Install-Module az -Force
 Import-Module az -Force
 Install-Module azuread -Force
@@ -31,7 +53,9 @@ Select-AzSubscription -SubscriptionId $SubscriptionId
 $TenantId = $subscription.TenantId
 Connect-AzureAD -TenantId $TenantId
 
-#Register Provider
+######################
+# Register Provider #
+######################
 Register-AzResourceProvider -ProviderNamespace Microsoft.KeyVault
 Register-AzResourceProvider -ProviderNamespace Microsoft.Network
 Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
@@ -39,52 +63,46 @@ Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
 Register-AzResourceProvider -ProviderNamespace Microsoft.insights
 Register-AzResourceProvider -ProviderNamespace Microsoft.DesktopVirtualization
 
-# Read the JSON file of the Custom Role
-#Change the letters by the letter you want to use 
-$jsonContent = Get-Content -Path C:\AVD-Image-Custom-role.json | ConvertFrom-Json
-
-# Modify the value of the custom role for the Azure Subscription with the subscription id get previously
+#############################################
+# Modify the Json Script with the Variable #
+#############################################
+$jsonContent = Get-Content -Path C:\AvdImage\AVD-Image-Custom-role.json | ConvertFrom-Json
 $jsonContent.AssignableScopes = $jsonContent.AssignableScopes -replace "{subscriptionId}", $SubscriptionId
 $jsonContent.Name = $NameoftheAvdImageCustomRole
+$jsonContent | ConvertTo-Json | Set-Content -Path C:\AvdImage\AVD-Image-Custom-role.json
 
-# Write the updated JSON back to the file
-$jsonContent | ConvertTo-Json | Set-Content -Path C:\AVD-Image-Custom-role.json
-
-# Read the JSON file of the Managed Identity
-$jsonContent2 = Get-Content -Path C:\AVD-Image-User-Managed-identity.json | ConvertFrom-Json
-
-# Update the values of the Managed Identity with the variable set previously
+$jsonContent2 = Get-Content -Path C:\AvdImage\AVD-Image-User-Managed-identity.json | ConvertFrom-Json
 $jsonContent2.resources[0].name = $UserManagedIdName
 $jsonContent2.resources[0].location = $ResourceLocation
 
-# Read the JSON file of the Azure Compute Gallery
-$jsonContent3 = Get-Content -Path C:\AVD-Azure-Compute-Gallery | ConvertFrom-Json
-
-# Update the values of the Managed Identity with the variable set previously
+$jsonContent3 = Get-Content -Path C:\AvdImage\AVD-Azure-Compute-Gallery.json | ConvertFrom-Json
 $jsonContent3.resources[0].name = $AvdAzureComputeGalleryName
 $jsonContent3.resources[0].location = $ResourceLocation
 
-# Create the custom role from the JSON file
-#Change the letters by the letter you want to use
+######################################
+# Creation of the Azure Custom Role #
+######################################
 New-AzRoleDefinition -InputFile C:\AVD-Image-Custom-role.json
-#Congratulation the Custom role is created 
 
-#Deploy the ARM template to create the User managed identity
+##########################################
+# Creation of the User managed identity #
+##########################################
 New-AzResourceGroup -Name $AVDImageResourceGroup -Location $ResourceLocation
 New-AzResourceGroupDeployment -ResourceGroupName $AVDImageResourceGroup -TemplateFile C:\AVD-Image-User-Managed-identity.json
-#Congratulation the User Managed Identity is created 
 
-#Assign the Custom Role the User Managed Id
+###############################################
+# Assign the Custom Role the User Managed Id #
+###############################################
 Install-Module Az.ManagedServiceIdentity -Force
 Import-Module Az.ManagedServiceIdentity -Force
 $AvdImageCustomRole = Get-AzRoleDefinition $NameoftheAvdImageCustomRole
-$userManagedIdentity = Get-AzUserAssignedIdentity -ResourceGroupName $resourceGroupName -Name $identityName
+$userManagedIdentity = Get-AzUserAssignedIdentity -ResourceGroupName $AVDImageResourceGroup -Name $UserManagedIdName
 $UserManagedIdentityId = $userManagedIdentity.Id
 $AvdImageCustomRoleScope = "/subscriptions/$SubscriptionId/resourceGroups/$UserManagedIDResourceGroup/providers/Microsoft.ManagedIdentity/userAssignedIdentities/$UserManagedIdName"
 New-AzRoleAssignment -ObjectId $UserManagedIdentityId -RoleDefinitionName $AvdImageCustomRole.Name -Scope $AvdImageCustomRoleScope
-#Congratulation the Custom Role has been assigned to the User Managed Identity
 
-#Deploy the ARM template to create the Azure Compute Gallery
+##########################################
+# Creation of the Azure Compute Gallery #
+##########################################
 New-AzResourceGroup -Name $AVDImageResourceGroup -Location $ResourceLocation
 New-AzResourceGroupDeployment -ResourceGroupName $AVDImageResourceGroup -TemplateFile C:\AVD-Azure-Compute-Gallery
-#Congratulation the Azure Compute Gallery is created 
